27bdd9197b809bbda4104b85ed8d1b18d92077be
jahnavipadala-01 <jahnavipadala123@gmail.com>
2026-07-07 13:16:03 +0530

    Add files via upload

---
+++ J3PS.java
@@ -0,0 +1,29 @@
+public class J3PS {
+    public static void main(String[] args) {
+        // int a = 6*5-34/2;
+        /*
+            =30-34/2
+            =30 - 17
+            =13
+        */
+        // int b = 60/5 - 34*2;
+        /*
+            =12 -34*2
+            =12 - 68
+            =-56
+        */
+        // System.out.println(a);
+        // System.out.println(b);
+        // Precedence & Associativity
+        // Quick Quiz //
+        // int x = 6;
+        // int y = 1;
+        // int k = x * y/2;
+        int b = 1;
+        int c = 2;
+        int a = 5;
+        int k = b*b - (4*a*c)/(2*a);
+        System.out.println(k);
+
+    }
+}
