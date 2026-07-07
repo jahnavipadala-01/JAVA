3808914ed4b321827e402a9aea5579fc2db58bd7
jahnavipadala-01 <jahnavipadala123@gmail.com>
2026-07-07 13:07:28 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,15 @@
+
+public class forloop {
+    public static void main(String[] args) {
+        // for(int counter = 0; counter < 5;counter = counter + 1){
+        //     System.out.println("I'm Jahnavi");
+        // }
+
+        // print 1-10
+        for(int i = 1; i <=10; i++){//i++ => i = counter + 1
+            // System.out.println(i);
+            // System.out.println(i);
+            System.out.print(i+" ");
+        }
+    }
+}
