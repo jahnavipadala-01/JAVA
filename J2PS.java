aeb9d1275e6c2e0c81c59276aeb7fb89365d33cf
jahnavipadala-01 <jahnavipadala123@gmail.com>
2026-07-07 13:15:13 +0530

    Add files via upload

---
+++ J2PS.java
@@ -0,0 +1,28 @@
+public class J2PS {
+    public static void main(String[] args) {
+        // PROBLEM-1
+        // String name = "Jack Parker";
+        // name = name.toLowerCase();
+        // System.out.println(name);
+
+        // PROBLEM -2
+        // String text = "To My Future Self";
+        // text = text.replace(" ", "_");
+        // System.out.println(text);
+
+        // PROBLEM-3
+        // String letter = "Dear <|name|>, Thanks a lot!";
+        // letter = letter.replace("<|name|>", "Jahnavi");
+        // System.out.println(letter);
+
+        // PROBLEM-4
+        // String myString = "This string contains  double and  triple spaces";
+        // System.out.println(myString.indexOf("  "));
+        // System.out.println(myString.indexOf("   "));
+
+        //PROBLEM-5
+        String letter2 = "Dear Jahnavi, \n\tThis Java course is Nice.\n\tThanks!";
+        System.out.println(letter2);
+    }
+    
+}
