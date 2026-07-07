1d68a1f4420f94599e34cea34b61d376815d5f54
jahnavipadala-01 <jahnavipadala123@gmail.com>
2026-07-07 13:09:48 +0530

    Add files via upload

---
+++ J1PS.java
@@ -0,0 +1,46 @@
+public class J1PS {
+    public static void main(String[] args) {
+
+                        //QUESTION - 1 
+        // int a = 8;
+        // int b = 9;
+        // int c = 5;
+        // int sum = a + b + c;
+
+        // System.out.println(sum);
+
+                        // QUESTION - 2
+
+            // float sub1 = 58;
+            // float sub2 = 67;
+            // float sub3 = 59;
+            // float cgpa = (sub1 + sub2 + sub3) / 30;
+            // System.out.println(cgpa);
+
+
+                        // QUESTION - 3
+    
+            // System.out.println("What is your name:");
+            // Scanner sc = new Scanner(System.in);
+            // String name = sc.next();
+            // System.out.println("Hello" + name + "have a good day");
+
+
+
+                    //QUESTION-
+
+                    //KILOMETERS TO MILES
+
+
+
+                    // QUESTION - 5
+
+                    // System.out.println("Enter your number");
+                    // Scanner sc = new Scanner(System.in);
+                    // System.out.println(sc.hasNextInt());
+
+    }
+    
+}
+
+
---
+++ Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+public static void main(String[] args) {
+    int a = 4;
+    // int b = 6 % a; // modulo operator
+    int b = 9;
+    b += 3;
+    a -= 1;
+    System.out.println(b);
+    System.out.println(a);
+    System.out.println(64>6); // comparison operators
+    System.out.println(98<6); // comparison operators
+    System.out.println(64>5 && 64<8); 
+    System.out.println(64>5 || 64>98); // logical operator
+
+}
+}
